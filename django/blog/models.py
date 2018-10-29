2d0ac0af48bd95a44981f3873ad57cc6b1c064de
pbzweihander <sd852456@naver.com>
2018-10-29 23:00:52 +0900

    Article, Comment model

--- django/blog/models.py
+++ django/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField(max_length=500)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.TextField(max_length=240)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} - {}".format(self.author, self.content)
