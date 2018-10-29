40c9e20066815c08ae99fb62257e394986de300c
pbzweihander <sd852456@naver.com>
2018-10-30 00:03:33 +0900

    get article test

--- django/blog/tests.py
+++ django/blog/tests.py
@@ -4,25 +4,6 @@ from django.contrib.auth.models import User
 import json
 
 
-class CsrfTestCase(TestCase):
-    def test_csrf(self):
-        # By default, csrf checks are disabled in test client
-        # To test csrf protection we enforce csrf checks here
-        client = Client(enforce_csrf_checks=True)
-        response = client.post('/api/signup', json.dumps({'username': 'chris', 'password': 'chris'}),
-                               content_type='application/json')
-        # Request without csrf token returns 403 response
-        self.assertEqual(response.status_code, 403)
-
-        response = client.get('/api/token')
-        # Get csrf token from cookie
-        csrftoken = response.cookies['csrftoken'].value
-
-        response = client.post('/api/signup', json.dumps({'username': 'chris', 'password': 'chris'}),
-                               content_type='application/json', HTTP_X_CSRFTOKEN=csrftoken)
-        self.assertEqual(response.status_code, 201)  # Pass csrf protection
-
-
 class BlogTestCase(TestCase):
     def setUp(self):
         self.reset_client()
@@ -46,6 +27,27 @@ class BlogTestCase(TestCase):
     def delete(self, url):
         return self.client.delete(url, HTTP_X_CSRFTOKEN=self.csrftoken)
 
+
+class CsrfTestCase(TestCase):
+    def test_csrf(self):
+        # By default, csrf checks are disabled in test client
+        # To test csrf protection we enforce csrf checks here
+        client = Client(enforce_csrf_checks=True)
+        response = client.post('/api/signup', json.dumps({'username': 'chris', 'password': 'chris'}),
+                               content_type='application/json')
+        # Request without csrf token returns 403 response
+        self.assertEqual(response.status_code, 403)
+
+        response = client.get('/api/token')
+        # Get csrf token from cookie
+        csrftoken = response.cookies['csrftoken'].value
+
+        response = client.post('/api/signup', json.dumps({'username': 'chris', 'password': 'chris'}),
+                               content_type='application/json', HTTP_X_CSRFTOKEN=csrftoken)
+        self.assertEqual(response.status_code, 201)  # Pass csrf protection
+
+
+class ModelTestCase(TestCase):
     def test_basic_models(self):
         new_user = User.objects.create_user(
             username='swpp', password='iluvswpp')
@@ -56,6 +58,8 @@ class BlogTestCase(TestCase):
                               content='Comment!', author=new_user)
         new_comment.save()
 
+
+class UserTestCase(BlogTestCase):
     def test_sign_up_in_and_out(self):
         resp = self.post('/api/signup',
                          {'username': 'rustacean', 'password': 'iluvrust'})
@@ -76,3 +80,39 @@ class BlogTestCase(TestCase):
 
         resp = self.get('/api/signout')
         self.assertEqual(resp.status_code, 401)
+
+
+class ArticleTestCase(BlogTestCase):
+    def setUp(self):
+        super().setUp()
+        user1 = User.objects.create_user(
+            username='user1', password='user1secret')
+        user2 = User.objects.create_user(
+            username='user2', password='user2secret')
+
+        article1 = Article(title='article1 title',
+                           content='article1 content', author=user1)
+        article1.save()
+        article2 = Article(title='article2 title',
+                           content="article2 content", author=user2)
+        article2.save()
+
+        comment1 = Comment(article=article2,
+                           content='comment1 content', author=user1)
+        comment1.save()
+        comment2 = Comment(article=article1,
+                           content='comment2 content', author=user2)
+        comment2.save()
+
+    def test_get_article(self):
+        resp = self.get('/api/article')
+        self.assertEqual(resp.status_code, 200)
+
+        resp_json = resp.json()
+        self.assertEqual(len(resp_json), 2)
+        self.assertEqual(resp_json[0].title, "article1 title")
+        self.assertEqual(resp_json[1].title, "article2 title")
+        self.assertEqual(resp_json[0].author_id, 0)
+        self.assertEqual(resp_json[1].author_id, 1)
+        self.assertEqual(resp_json[0].content, "article1 content")
+        self.assertEqual(resp_json[1].content, "article2 content")
