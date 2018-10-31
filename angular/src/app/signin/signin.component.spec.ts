13ca099ad8b66675d9c42e0a631751540c2936e9
pbzweihander <sd852456@naver.com>
2018-11-01 04:55:46 +0900

    signin component test

--- angular/src/app/signin/signin.component.spec.ts
+++ angular/src/app/signin/signin.component.spec.ts
@@ -1,16 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { SigninComponent } from './signin.component';
+import { UsersService } from '../users.service';
+import { Router } from '@angular/router';
+import { User } from '../user';
+
+const mockUser: User = {
+  id: 3,
+  email: 'user3@email.test',
+  password: 'user3password',
+  name: 'user3',
+  signed_in: false,
+};
 
 describe('SigninComponent', () => {
   let component: SigninComponent;
   let fixture: ComponentFixture<SigninComponent>;
+  let usersService: jasmine.SpyObj<UsersService>;
+  let router: jasmine.SpyObj<Router>;
+  let alert: jasmine.Spy;
+
 
   beforeEach(async(() => {
+    const usersSpy = jasmine.createSpyObj('UsersService', ['login']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
     TestBed.configureTestingModule({
-      declarations: [ SigninComponent ]
+      imports: [FormsModule],
+      declarations: [SigninComponent],
+      providers: [
+        { provide: UsersService, useValue: usersSpy },
+        { provide: Router, useValue: routerSpy },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+
+    usersService = TestBed.get(UsersService);
+    usersService.login.and.returnValue(new Promise(r => r(mockUser)));
+    router = TestBed.get(Router);
+    alert = spyOn(window, 'alert');
   }));
 
   beforeEach(() => {
@@ -22,4 +51,40 @@ describe('SigninComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not respone', async(() => {
+    component.submitting = true;
+    component.signin();
+    expect(usersService.login).toHaveBeenCalledTimes(0);
+  }));
+
+  it('should login', async(() => {
+    component.loginForm = {
+      email: 'foo',
+      password: 'bar',
+    };
+    component.signin();
+    expect(component.submitting).toBeTruthy();
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(router.navigate).toHaveBeenCalledWith(['/articles']);
+    });
+  }));
+
+  it('should not login', async(() => {
+    usersService.login.and.returnValue(new Promise(r => r(null)));
+    component.loginForm = {
+      email: 'foo',
+      password: 'bar',
+    };
+    component.signin();
+    expect(component.submitting).toBeTruthy();
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(alert).toHaveBeenCalled();
+      expect(component.submitting).toBeFalsy();
+    });
+  }));
 });
